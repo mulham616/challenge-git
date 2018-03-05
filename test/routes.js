e921851dcde35cf44e47d8b3a17b4aee12002181
mulhamadil <molhamadel10@outlook.com>
2018-03-05 08:56:36 -0800

    feat: add echo route

--- test/routes.js
+++ test/routes.js
@@ -84,6 +84,7 @@ tape('should get stream', function (t) {
     })
 })
 
+<<<<<<< HEAD
 tape('should get reverse', function (t) {
   var expected = {
     input: 'stringtoreverse',
@@ -91,11 +92,19 @@ tape('should get reverse', function (t) {
   }
 
   var url = '/reverse/' + expected.input
+=======
+tape('should get echo', function (t) {
+  var url = '/echo?one=1&two=2'
+>>>>>>> 7e86a19 (feat: add echo route)
   servertest(server(), url, {encoding: 'json'}, function (err, res) {
     t.ifError(err, 'no error')
 
     t.equal(res.statusCode, 200, 'correct statusCode')
+<<<<<<< HEAD
     t.deepEqual(res.body, expected, 'values should match')
+=======
+    t.deepEqual(res.body, {one: '1', two: '2'}, 'values should match')
+>>>>>>> 7e86a19 (feat: add echo route)
     t.end()
   })
 })
